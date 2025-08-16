b2b38b704f4cbd9fe74e7d60817508dcab2f2665
Nicholas Welch-Bauer <nicholas@welch-bauer.com>
2025-08-16 18:41:00 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,8 +7,7 @@ let package = Package(
     name: "Runestone",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v17),
-        .visionOS(.v2)
+        .iOS(.v14)
     ],
     products: [
         .library(name: "Runestone", targets: ["Runestone"])
